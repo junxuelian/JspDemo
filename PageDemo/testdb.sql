354b17b3b786357d6b1a8c9cf018f11476621f8d
junxuelian <917136891@qq.com>
2017-04-14 18:53:54 +0800

    testdb.sql

---
+++ PageDemo/testdb.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.1.62-community : Database - testdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `testdb`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `pwd` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`id`,`userName`,`pwd`) values (1,'jack','123456'),(2,'jack2','123456'),(3,'admin','admin'),(4,'admi','admin');
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `empId` int(11) NOT NULL DEFAULT '0',
+  `empName` varchar(20) DEFAULT NULL,
+  `dept_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`empId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `employee` */
+
+insert  into `employee`(`empId`,`empName`,`dept_id`) values (1,'Jack',1),(2,'Rose',2),(3,'李玉川',3),(4,'林记',4),(5,'Jack2',5),(6,'Rose2',6),(7,'李玉川2',7),(8,'林记2',8),(9,'Jack3',9),(10,'Rose3',10),(11,'李玉川3',11),(12,'林记3',12);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
